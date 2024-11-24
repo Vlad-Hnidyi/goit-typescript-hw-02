0f225c195e36fbffb18bd0c8bfa30b3c9eb9d408
Vlad Hnidyi <vgnedoj03@gmail.com>
2024-11-24 18:51:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+
+import { Toaster } from "react-hot-toast";
+
+import { requestPhotoByKey } from "./pics-api.js";
+
+import css from "./App.module.css";
+
+import ImageGallery from "./components/ImageGallery/ImageGallery.jsx";
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage.jsx";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
+import ImageModal from "./components/ImageModal/ImageModal.jsx";
+
+// import "./App.css";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [keyWord, setKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [modalImageInfo, setModalImageInfo] = useState({});
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const onOpenModal = (imgInfo) => {
+    setModalImageInfo(imgInfo);
+    setModalIsOpen(true);
+  };
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  const onSearch = (userWord) => {
+    setKeyword(userWord);
+    setError(false);
+    setPage(1);
+  };
+
+  const onLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (!keyWord) return;
+
+    const request = async () => {
+      try {
+        setLoading(true);
+        const response = await requestPhotoByKey(keyWord, page);
+
+        setTotalPages(response.data.total_pages);
+
+        setImages((prevImages) => {
+          if (page === 1) {
+            return response.data.results;
+          }
+
+          return [...prevImages, ...response.data.results];
+        });
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    request();
+  }, [keyWord, page]);
+
+  return (
+    <div className={css.appWrapper}>
+      <SearchBar onSearch={onSearch} />
+      {(images.length > 0 && (
+        <ImageGallery onOpenModal={onOpenModal} images={images} />
+      )) ||
+        (error && <ErrorMessage />)}
+      {loading && <Loader />}
+      {page < totalPages && <LoadMoreBtn onLoadMore={onLoadMore} />}
+      <Toaster />
+      <ImageModal
+        modalIsOpen={modalIsOpen}
+        modalImageInfo={modalImageInfo}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+}
+
+export default App;
